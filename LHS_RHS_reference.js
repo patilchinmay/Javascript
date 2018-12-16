ed960614e1e4032ff85a5435c4250dc4ca524cc2
chipatil <chipatil@cisco.com>
2018-12-16 20:03:41 +0530

    Nested Scope and LHS RHS References

---
+++ LHS_RHS_reference.js
@@ -0,0 +1,5 @@
+var a  = 12; // LHS reference. We don't care what the current value is. Simply find 'a' for assignent.
+
+console.log(a); // RHS reference. No value is eing assigned here.
+
+//Left or Right of "assignment operator"
