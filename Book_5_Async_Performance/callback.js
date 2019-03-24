bc410fc9f76c83ba04b851456195bc633839e91c
Chinmay <patilchinmay01@gmail.com>
2019-03-24 23:20:43 +0530

    Callback

---
+++ Book_5_Async_Performance/callback.js
@@ -0,0 +1,10 @@
+const time = Math.floor(Math.random() * 3001);     // returns a random integer from 0 to 3000 milisecond
+console.log(time);
+
+function foo(){
+    console.log("This has been called via a callback after "+ time +" milisecond!");
+}
+
+setTimeout(foo, time);
+
+// time variable is resolved inside foo because of traversing the scope chain to find the parameter.
