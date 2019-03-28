7f8f4f51473161df9cf81348dff8cdb9a33a7755
Chinmay <patilchinmay01@gmail.com>
2019-03-28 21:50:31 +0530

    Iterator example

---
+++ Book_5_Async_Performance/iterator.js
@@ -0,0 +1,17 @@
+// https://www.youtube.com/watch?v=ZNrJPzjNt-o
+
+let i = [1,2,3,4];
+
+let iterator = i[Symbol.iterator]();
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+// { value: 1, done: false }
+// { value: 2, done: false }
+// { value: 3, done: false }
+// { value: 4, done: false }
+// { value: undefined, done: true }
