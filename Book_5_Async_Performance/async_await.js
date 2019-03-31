87913c8f622ff24bf8ac90a50bfa462310526d6f
Chinmay <patilchinmay01@gmail.com>
2019-03-31 16:24:57 +0530

    Async-await demo

---
+++ Book_5_Async_Performance/async_await.js
@@ -0,0 +1,39 @@
+// async-await works with prmoises. So await should be used in conjuction with the promises.
+
+function add(x,y){
+    return new Promise((resolve, reject)=>{
+        setTimeout(function(){
+            resolve (x+y);
+        }, 2000)
+    });   
+}
+
+function square(z){
+   return new Promise((resolve, reject)=>{
+        setTimeout(function(){
+            resolve (z*z);
+        }, 2000)
+   });
+}
+
+function double(w){
+    return w+w;
+}
+
+let generatorFunction = async() => {
+    try{
+        let sum = await add(2,4);
+        console.log(sum);
+
+        let squared = await square(sum);
+        console.log(squared);
+
+        let doubled = double(squared);
+        console.log(doubled);
+
+    }catch(err){
+        console.error(err);
+    }
+}
+
+generatorFunction();
